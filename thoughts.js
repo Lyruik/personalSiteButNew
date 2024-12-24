6912f731fe4358743f80da28228a1f5f20766400
michael.wadsworth <mwadsworthdev@gmail.com>
2024-12-24 12:09:47 -0700

    setting up where blocks go in the site a bit, focusing on the 'skeleton', then putting data into it, then make it pretty

--- thoughts.js
+++ thoughts.js
@@ -11,18 +11,38 @@
  */
 
 const bigProblem = {
-  whoAreYou: {
-    placement: {
+  infoBlock: {
+    basicPlacement: {
       desc: "where to put things?",
+      status: 1,
+      note:
+        "putting all info like 'who are you' and 'what do you do' on the left side 30%~",
+    },
+    advancedPlacement: {
+      desc: "make things look pretty",
       status: 0,
-      note: "prolly blocks 1 and 2 on left 40~ percent",
+      note:
+        "between name, title, bio, and links, how to space it out best and look nice",
+    },
+    infoContent: {
+      desc: "what to put in blocks",
+      status: 0,
+      note:
+        "do we want more than what is already there? we should probably make it sound better and have better info",
     },
   },
-  whatDoYouDo: {
+  projectJourneyBlock: {
+    layout: {
+      desc: "how to display this info",
+      status: 0,
+      note:
+        "going with most of the page being a content box, one will show projects and the other 'things i have learned'",
+    },
     content: {
       desc: "what to put here? like our projects, but previews and stuff too?",
       status: 0,
-      note: "",
+      note:
+        "Make some bars of content that reflect how they are on the resume for projects, have a lil picture, etc. Then for journey something very similar to old personal site where it can have images and descriptions. Generally will be like 'did you know you can do this? see it in screenshot? also explained in this lil text box'",
     },
   },
 };
